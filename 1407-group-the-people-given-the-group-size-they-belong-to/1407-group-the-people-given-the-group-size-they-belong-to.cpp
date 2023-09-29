b057eff9ba5f74ed18b2134920941ba210240ad9
Gaurav kholiya <114013077+Gauravvkholiya@users.noreply.github.com>
2023-09-30 00:39:27 +0530

    Time: 13 ms (29.07%), Space: 12.8 MB (67.96%) - LeetHub

--- 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -5,7 +5,7 @@ public:
         vector<vector<int>>result;
         int n = groupSizes.size();
         int index;
-        for(int i =0;i<n;++i)
+        for(int i =0;i<n;i++)
         {
             index = groupSizes[i];
             mpp[index].push_back(i);
