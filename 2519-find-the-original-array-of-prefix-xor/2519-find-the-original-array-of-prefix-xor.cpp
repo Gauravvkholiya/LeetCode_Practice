99980407becc4f99355460bc31dec1195b185585
Gaurav kholiya <114013077+Gauravvkholiya@users.noreply.github.com>
2023-09-30 10:48:11 +0530

    Time: 58 ms (99.46%), Space: 79.5 MB (16.56%) - LeetHub

--- 2519-find-the-original-array-of-prefix-xor/2519-find-the-original-array-of-prefix-xor.cpp
+++ 2519-find-the-original-array-of-prefix-xor/2519-find-the-original-array-of-prefix-xor.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<int> findArray(vector<int>& pref) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         vector<int>ans;
         ans.push_back(pref[0]);
         int n = pref.size();
