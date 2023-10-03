c9f430af6c879002d2aec5db44b73bd976721da4
Gaurav kholiya <114013077+Gauravvkholiya@users.noreply.github.com>
2023-10-04 02:19:29 +0530

    Time: 0 ms (100%), Space: 6.9 MB (10.71%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int n = word1.size();
         int m = word2.size();
         int i = 0;
