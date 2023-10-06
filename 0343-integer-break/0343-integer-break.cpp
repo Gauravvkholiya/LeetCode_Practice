e46d03567e8bae90db7e3cc9d5da5eaadfa88985
Gaurav kholiya <114013077+Gauravvkholiya@users.noreply.github.com>
2023-10-07 01:52:03 +0530

    Time: 2 ms (50.23%), Space: 6.4 MB (37.04%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,6 +1,8 @@
  class Solution {
     public :
     int integerBreak(int n) {
+        cin.tie(0);
+        cout.tie(0);
         if(n==2) return 1;
         if(n==3) return 2;
         int product = 1;
