ba0153b0b79e8e15a3b79af799ef118ed9f945db
Gaurav kholiya <114013077+Gauravvkholiya@users.noreply.github.com>
2023-10-14 23:57:39 +0530

    Time: 3 ms (86.76%), Space: 21.2 MB (18.26%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+
+        //optimal solution : reducing the space 
+        //T.C = O(n) && S.C = O(1) 
+        int n = s.size();
+        string temp = "";
+        string ans = "";
+        for(int i =0;i<n;i++)
+        {
+            if(s[i]==' ')
+            {
+                continue;
+            }
+            while(s[i]!=' '&& i<n)
+            {
+                temp +=s[i];
+                i++;
+            }
+            if(ans=="")
+            {
+                ans+=temp;
+                temp="";
+            }
+            else{
+                ans= temp+" "+ans;
+                temp="";
+            }
+        }
+        return ans;
+
+        //brute force:--
+        // T.C = O(n)
+        //S.C = O(n)
+        // string temp="";
+        // stack<string> st;
+        // string ans;
+        // int n = s.size();
+        // for(int i = 0;i<n;i++)
+        // {
+        //     if(s[i]==' ')
+        //     {
+        //         continue;
+        //     }
+        //     while(i<n&&s[i]!=' ')
+        //     {
+        //         temp+=s[i];
+        //         i++;
+        //     }
+        //     st.push(temp);
+        //     temp="";
+        // }
+
+        // while(st.size()!=1)
+        // {
+        //     ans += st.top()+" ";
+        //     st.pop();
+        // }
+        // ans+=st.top();
+        // return ans;
+
+    }
+};
