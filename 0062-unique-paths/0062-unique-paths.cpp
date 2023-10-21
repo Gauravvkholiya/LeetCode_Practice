b41c272869e9e856aa0a2525d8320024b8f9b1cd
Gaurav kholiya <114013077+Gauravvkholiya@users.noreply.github.com>
2023-10-21 12:20:40 +0530

    Time: 0 ms (100%), Space: 6.9 MB (5.41%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,24 +1,7 @@
 class Solution {
 public:
-    int find(int m ,int n, vector<vector<int>>dp)
-    {
-        if(m==0&&n==0)
-        {
-            return 1;
-        }
-        if(m<0||n<0)
-        {
-            return 0;
-        }
-        if(dp[m][n]!=-1)
-        {
-            return dp[m][n];
-        }
-        int up = find(m-1,n,dp);
-        int left =find(m,n-1,dp);
-        return dp[m][n] =  up +left;
-    }
     int uniquePaths(int m, int n) {
+        cin.tie(0);
         vector<vector<int>>dp(m,vector<int>(n,1));
         dp[0][0] = 1;
         int up ,left;
