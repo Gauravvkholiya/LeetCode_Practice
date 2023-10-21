a5910a45f1297df0344d1418add5b8facfa21d89
Gaurav kholiya <114013077+Gauravvkholiya@users.noreply.github.com>
2023-10-21 12:40:18 +0530

    Time: 0 ms (100%), Space: 6.9 MB (9.61%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -3,17 +3,17 @@ public:
     int uniquePaths(int m, int n) {
         cin.tie(0);
         vector<vector<int>>dp(m,vector<int>(n,1));
-        dp[0][0] = 1;
-        int up ,left;
+        dp[0][0]=1;
+        int up,left;
         for(int i =1;i<m;i++)
         {
             for(int j =1;j<n;j++)
             {
-                    if(i>0)
-                    up = dp[i][j-1];
-                    if(j>0)
-                    left = dp[i-1][j];
-                    dp[i][j] = left+up;
+                if(i>0)
+                up = dp[i][j-1];
+                if(j>0)
+                left = dp[i-1][j];
+                dp[i][j] = up+left;
             }
         }
         return dp[m-1][n-1];
